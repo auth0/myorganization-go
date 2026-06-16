f0e468628a2e840bd3f91696f6860ae65dfe4be5
Kunal Dawar <kunal.dawar@okta.com>
2026-06-16 19:34:06 +0530

    Release: v1.0.0

--- meta.go
+++ meta.go
@@ -4,4 +4,4 @@ package myorganization
 const SDKName = "myorganization-go"
 
 // SDKVersion is the version of the myorganization-go SDK.
-const SDKVersion = "v1.0.0-beta.0"
+const SDKVersion = "v1.0.0"
